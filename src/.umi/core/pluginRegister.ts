7b52038e9b575891ef4f6dccd680c9c497f5021f
klakshmideepthi <kurugundla01@gmail.com>
2021-07-31 16:49:06 +0530

    initial commit-18

---
+++ src/.umi/core/pluginRegister.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+import { plugin } from './plugin';
+import * as Plugin_0 from 'C:/Users/Deepthi/Desktop/ph_4climate/src/app.js';
+
+  plugin.register({
+    apply: Plugin_0,
+    path: 'C:/Users/Deepthi/Desktop/ph_4climate/src/app.js',
+  });
+
+export const __mfsu = 1;
